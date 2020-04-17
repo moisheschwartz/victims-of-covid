8e02689e0322226b320ed0fdc99999bd094b3fae
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-17 12:57:04 -0400

    Added Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -9,7 +11,12 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
+
 );
+
+ReactGA.initialize('UA-163913932-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
