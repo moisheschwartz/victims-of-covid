007ae9e1e5f55431da44c9bb85048fe339093d19
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-19 12:45:44 -0400

    Added Countup to total

--- src/components/campaigns/campaignsTotal.js
+++ src/components/campaigns/campaignsTotal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import CountUp from 'react-countup';
 
 var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -13,7 +14,17 @@ export default function CampaignsTotal(props) {
 
     return (
         <div style={{ textAlign: "center", fontSize: "30px", margin: "23px 0 " }}>
-            Together We Raised:  <br /><span className={"totalRaised"}>{formatter.format(total)}</span>
+
+
+
+            Together We Raised:  <br /><span className={"totalRaised"}>{<CountUp
+                start={0}
+                end={total}
+                duration={2}
+                separator=","
+                decimals={2}
+                prefix="$"
+            />}</span>
             <br />!מי כעמך ישראל
         </div>
     )
