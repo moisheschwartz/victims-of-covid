6c71ae1c97fb9c192274c0757c006405b0f6ea80
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-19 00:29:44 -0400

    Removed Live sync note

--- src/components/footer/footerBar.js
+++ src/components/footer/footerBar.js
@@ -5,7 +5,7 @@ export default function FooterBar() {
     return (
         <>
             <div style={{ padding: "12px 20px", width: "100%" }}>
-                <p style={{ textAlign: "center" }}>Please note: The data is not updated in real time, it's done manually approx. once a day if the site will gain traction I'll work on making it sync the totals automaticlly every few hours</p>
+                <p style={{ textAlign: "center" }}></p>
             </div>
             <div style={{ padding: "12px 20px", width: "100%", backgroundColor: "teal", textAlign: "center", justifyContent: "space-around", color: "#fff" }}>
                 <div>Created by Moishe Schwartz. <br />Suggestions, Comments or anything else please email <a style={{ color: "#fff" }} href="mailto:mosesschwartzdev@gmail.com">mosesschwartzdev@gmail.com</a></div>
