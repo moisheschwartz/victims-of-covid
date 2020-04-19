acbd55a822ad07eaf44bacf5684f1874c7732fca
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-18 22:44:56 -0400

    Fixed Upper/Lower case search issue

--- src/components/campaigns/campaigns.js
+++ src/components/campaigns/campaigns.js
@@ -9,7 +9,7 @@ export default function Campaigns(props) {
             <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
                 <SearchBar handleSearch={props.handleSearch} />
 
-                {list.records.filter(r => r.fields.Name.includes(searchKeyword)).map(c => <Campaign campaign={
+                {list.records.filter(r => r.fields.Name.toLowerCase().includes(searchKeyword.toLowerCase())).map(c => <Campaign campaign={
                     {
                         name: c.fields.Name,
                         total: c.fields.Total,
