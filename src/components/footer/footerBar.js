a6c2bc5e32c6da464d64265e70be717e9ccc9294
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-19 01:21:58 -0400

    Added Kofi

--- src/components/footer/footerBar.js
+++ src/components/footer/footerBar.js
@@ -9,6 +9,8 @@ export default function FooterBar() {
             </div>
             <div style={{ padding: "12px 20px", width: "100%", backgroundColor: "teal", textAlign: "center", justifyContent: "space-around", color: "#fff" }}>
                 <div>Created by Moishe Schwartz. <br />Suggestions, Comments or anything else please email <a style={{ color: "#fff" }} href="mailto:mosesschwartzdev@gmail.com">mosesschwartzdev@gmail.com</a></div>
+                <br />
+                <a href='https://ko-fi.com/H2H61MLKY' target='_blank'><img height='36' style={{ 'border': '0px', 'height': '36px' }} src='https://cdn.ko-fi.com/cdn/kofi3.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
             </div>
         </>
     )
