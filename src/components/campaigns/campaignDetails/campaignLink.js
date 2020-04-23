ffcdb97a720de7911f0bcb27aa2ba890fa9ffbc1
moisheschwartz <schwartzmoishey@gmail.com>
2020-04-23 07:06:23 -0400

    Added tracking for outbound links

--- src/components/campaigns/campaignDetails/campaignLink.js
+++ src/components/campaigns/campaignDetails/campaignLink.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaLink } from 'react-icons/fa';
 import FaIcon from '../../other/faIcon';
+import ReactGA from 'react-ga';
 
 
 
 export default function CampaignLink(props) {
 
     const { link } = props;
+
+    useEffect(() => {
+        ReactGA.initialize('UA-163913932-1');
+
+    });
     return (
         <div className={"campaignLink"}>
 
             <FaIcon>
                 <FaLink />
             </FaIcon>
-            <a target="_blank" href={link}>{link}</a>
+            <ReactGA.OutboundLink eventLabel={link} to={link} target="_blank">
+                {link}            </ReactGA.OutboundLink>
+
         </div>
     )
 }
