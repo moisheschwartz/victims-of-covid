c891485fa4cd0468d8a0896106ecb8a9a2b129f5
moisheess <mschwartz@evelt.com>
2020-05-13 22:28:50 -0400

    Added "Donate: and "Copy link" button

--- src/components/campaigns/campaignDetails/campaignLink.js
+++ src/components/campaigns/campaignDetails/campaignLink.js
@@ -1,24 +1,20 @@
-import React, { useEffect } from 'react'
-import { FaLink } from 'react-icons/fa';
-import FaIcon from '../../other/faIcon';
-import ReactGA from 'react-ga';
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-
+import ReactGA from "react-ga";
 
 export default function CampaignLink(props) {
+  const { link } = props;
+  const [copied, setCopied] = useState(false);
+  return (
+    <div className={"campaignLink"}>
+      <ReactGA.OutboundLink eventLabel={link} to={link} target="_blank">
+        {"Donate"}
+      </ReactGA.OutboundLink>
 
-    const { link } = props;
-
-
-    return (
-        <div className={"campaignLink"}>
-
-            <FaIcon>
-                <FaLink />
-            </FaIcon>
-            <ReactGA.OutboundLink eventLabel={link} to={link} target="_blank">
-                {link}            </ReactGA.OutboundLink>
-
-        </div>
-    )
+      <CopyToClipboard text={link} onCopy={() => setCopied(true)}>
+        <div className={"copyLink"}>{copied ? "Copied" : "Copy Link"}</div>
+      </CopyToClipboard>
+    </div>
+  );
 }
